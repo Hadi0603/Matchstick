e0a948af48ee34c2a4b81c0b433bf9f8ffa69679
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2025-06-30 16:45:32 +0500

    Update SpotOccupied.cs

--- Assets/Scripts/SpotOccupied.cs
+++ Assets/Scripts/SpotOccupied.cs
@@ -1,6 +1,13 @@
+using System;
 using UnityEngine;
 
 public class SpotOccupied : MonoBehaviour
 {
+    [HideInInspector]
     public bool isOccupied = false;
+
+    private void Start()
+    {
+        isOccupied = transform.childCount != 0;
+    }
 }
